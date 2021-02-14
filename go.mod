53f0b0da735446ab8d1878ceb6fcc4e2b0607889
Markus Steinbach <ge57xip@tum.de>
2021-02-14 22:45:33 +0100

    implemented processor logic

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/koelschkellerkind/owtracecollector
 go 1.15
 
 require (
+	github.com/apache/openwhisk-client-go v0.0.0-20210106144548-17d556327cd3
+	github.com/cloudfoundry/jibber_jabber v0.0.0-20151120183258-bcc4c8345a21 // indirect
+	github.com/hokaccha/go-prettyjson v0.0.0-20210113012101-fb4e108d2519 // indirect
+	github.com/nicksnyder/go-i18n v1.10.1 // indirect
 	github.com/stretchr/testify v1.7.0
 	go.opentelemetry.io/collector v0.20.0
 )
--- processor.go
+++ processor.go
@@ -2,7 +2,11 @@ package owtraceprocessor
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
+	"github.com/apache/openwhisk-client-go/whisk"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/consumer/pdata"
@@ -19,6 +23,90 @@ func newOwTraceProcessor(next consumer.TracesConsumer) *owTraceProcessor {
 }
 
 func (s *owTraceProcessor) ConsumeTraces(ctx context.Context, batch pdata.Traces) error {
+	
+	config := &whisk.Config{
+		Host: "10.195.5.240:31001",
+		AuthToken: "23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP",
+		Insecure: true,
+	}
+	client, err := whisk.NewClient(http.DefaultClient, config)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		// ResourceSpans[] -> InstrumentationLibrarySpans[] -> Spans[]
+		for i := 0; i < batch.ResourceSpans().Len(); i++ {
+			rs := batch.ResourceSpans().At(i);
+			for j := 0; j < rs.InstrumentationLibrarySpans().Len(); j++ {
+				ils := rs.InstrumentationLibrarySpans().At(j);
+				
+				newSpans := pdata.NewSpanSlice()
+				for k := 0; k < ils.Spans().Len(); k++ {
+					executionSpan := ils.Spans().At(k)
+					id, ok := executionSpan.Attributes().Get("activationId")
+					if ok {
+						activation, res, err := client.Activations.Get(id.StringVal()) // TODO: implement retry
+						if err != nil {
+							fmt.Println(res, err)
+						}
+						if res.StatusCode == http.StatusOK {
+							executionStart := activation.Start
+							waitTime := activation.Annotations.GetValue("waitTime")
+							initTime := activation.Annotations.GetValue("initTime")
+
+							var waitTimeVal int64
+							if waitTime != nil {
+								waitTimeVal, _ = waitTime.(json.Number).Int64()
+							}
+							var initTimeVal int64
+							if initTime != nil {
+								initTimeVal, _ = initTime.(json.Number).Int64()
+							}
+
+							// create new parent span
+							newParentSpan := pdata.NewSpan()
+							executionSpan.CopyTo(newParentSpan)
+							newParentSpan.SetSpanID(executionSpan.SpanID())
+							newParentSpan.SetStartTime(pdata.TimestampUnixNano(executionStart - waitTimeVal - initTimeVal))
+							newSpans.Append(newParentSpan)
+		
+							// add execution span to new parent span
+							executionSpan.SetSpanID(pdata.NewSpanID([8]byte{1, 2, 3, 4, 5, 6, 7, 8}))
+							executionSpan.SetParentSpanID(newParentSpan.SpanID())
+							executionSpan.SetName("execution: " + executionSpan.Name())
+
+							// if wait time present: create wait span and add to new parent span
+							if waitTime != nil {
+								waitSpan := pdata.NewSpan()
+								executionSpan.CopyTo(waitSpan)
+								waitSpan.SetSpanID(pdata.NewSpanID([8]byte{2, 3, 4, 5, 6, 7, 8, 9}))
+								waitSpan.SetParentSpanID(newParentSpan.SpanID())
+								waitSpan.SetName("waitTime: " + waitSpan.Name())
+								waitSpan.SetStartTime(pdata.TimestampUnixNano(executionStart - initTimeVal - waitTimeVal))
+								waitSpan.SetEndTime(pdata.TimestampUnixNano(executionStart - initTimeVal))
+								newSpans.Append(waitSpan)
+							}
+							
+							// if init time present: create init span and add to new parent span
+							if initTime != nil {
+								initSpan := pdata.NewSpan()
+								executionSpan.CopyTo(initSpan)
+								initSpan.SetSpanID(pdata.NewSpanID([8]byte{3, 4, 5, 6, 7, 8, 9, 0}))
+								initSpan.SetParentSpanID(newParentSpan.SpanID())
+								initSpan.SetName("initTime: " + initSpan.Name())
+								initSpan.SetStartTime(pdata.TimestampUnixNano(executionStart - initTimeVal))
+								initSpan.SetEndTime(pdata.TimestampUnixNano(executionStart))
+								newSpans.Append(initSpan)
+							}
+						}
+					}
+				}
+				newSpans.MoveAndAppendTo(ils.Spans())
+			}
+		}
+	}
+	if err := s.next.ConsumeTraces(ctx, batch); err != nil {
+		fmt.Println(err)
+	}
 	return nil
 }
 
